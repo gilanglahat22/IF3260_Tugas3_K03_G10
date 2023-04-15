228f25b3e1e95a3b4f3c012163ad434bb6cc6d8c
Brianaldo <brianaldophandiarta31@gmail.com>
2023-04-15 16:25:13 +0700

    feat: setup webgl and migrate html from previous tubes

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
-let a = "test123";
+const canvas = document.getElementById(`gl-canvas`);
+const gl = WebGLUtils.setupWebGL(canvas, {
+    preserveDrawingBuffer: true,
+});
+
+if (!gl) {
+    console.error("WebGL isn't available");
+    alert("WebGL isn't available");
+}
+
+canvas.width = innerHeight;
+canvas.height = innerHeight;
+
+gl.viewport(0, 0, canvas.width, canvas.height);
+gl.clearColor(0.0, 0.0, 0.0, 0.66);
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+gl.enable(gl.DEPTH_TEST);
+gl.frontFace(gl.CCW);
+gl.cullFace(gl.BACK);
